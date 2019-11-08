a73d13b706c4b1bd855da2d26becbf484a84f525
Yann Renaudin <renaudin.yann@gmail.com>
2019-11-08 18:14:04 -0500

    feat: ref Home component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Home } from './views/Home.view';
 import './App.css';
+import { theme } from './ui/theme';
+import { ThemeProvider } from '@material-ui/styles';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Home />
+      </div>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
