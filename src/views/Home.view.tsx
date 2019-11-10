d9e8460d9f720bfa051eda433f04fe78ab0a18b9
Yann Renaudin <renaudin.yann@gmail.com>
2019-11-10 00:09:17 -0500

    refactor: home view display node id and provide actions to BFS, DFS, Reset indexes

--- src/views/Home.view.tsx
+++ src/views/Home.view.tsx
@@ -1,14 +1,15 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import { HierarchyPointNode } from 'd3';
 import { Button, Box, Typography } from '@material-ui/core';
 import { TreeViz } from '../components/TreeViz';
-import { TreeNode } from '../components/Tree';
 import { treeWithGenerators } from '../components/seeder';
+import { TreeNode, Employee } from '../components/types';
+import uuid from 'uuid/v4';
 
 export const Home: FC = () => {
-  const [selectedNode, setSelectedNode] = useState<TreeNode>();
-  const [root, setRootNode] = useState<TreeNode>(treeWithGenerators.root);
-  const [d3Tree, setD3Tree] = useState<HierarchyPointNode<TreeNode>>();
+  const [selectedNode, setSelectedNode] = useState<TreeNode<Employee>>();
+  const [root, setRootNode] = useState<TreeNode<Employee>>(treeWithGenerators.root);
+  const [d3Tree, setD3Tree] = useState<HierarchyPointNode<TreeNode<Employee>>>();
   return (
     <Box display="flex">
       <Box display="flex" flex={1} justifyContent="center" alignItems="center">
@@ -23,7 +24,7 @@ export const Home: FC = () => {
               Tree informations
             </Typography>
             <Typography component="span" color="textPrimary">
-              Number of nodes: {treeWithGenerators.size}
+              {/* Number of nodes: {treeWithGenerators.size} */}
             </Typography>
             <Typography component="span" color="textPrimary">
               Tree height: {d3Tree ? d3Tree.height : null}
@@ -35,6 +36,12 @@ export const Home: FC = () => {
             <Typography component="h3" color="primary">
               Selected node
             </Typography>
+            <Box>
+              <Typography component="span" color="textPrimary">
+                Id:
+              </Typography>
+              {selectedNode ? selectedNode.data.id : null}
+            </Box>
             <Box>
               <Typography component="span" color="textPrimary">
                 Role:
@@ -57,12 +64,43 @@ export const Home: FC = () => {
             <Button
               onClick={() => {
                 if (selectedNode) {
-                  treeWithGenerators.add({ name: 'yann', age: 30, role: 'Senior Software Engineer' }, selectedNode.data);
+                  treeWithGenerators.add({ id: uuid(), name: 'yann', role: 'Senior Software Engineer' }, selectedNode.data);
                   setRootNode({ ...treeWithGenerators.root });
                 }
               }}
             >
               Add node
+            </Button>
+            <Button
+              onClick={() => {
+                treeWithGenerators.cleanIndexes();
+                setRootNode({ ...treeWithGenerators.root });
+              }}
+            >
+              Reset indexes
+            </Button>
+
+            <Button
+              onClick={() => {
+                Array.from(treeWithGenerators.traverseBFS()).forEach(node => {
+                  console.log(`Visited ${node.data.index}. ${node.data.name}`);
+                });
+
+                setRootNode({ ...treeWithGenerators.root });
+              }}
+            >
+              Breadth first search
+            </Button>
+
+            <Button
+              onClick={() => {
+                Array.from(treeWithGenerators.traverseDFS()).forEach(node => {
+                  console.log(`Visited ${node.data.index}. ${node.data.name}`);
+                });
+                setRootNode({ ...treeWithGenerators.root });
+              }}
+            >
+              Depth first search
             </Button>
           </Box>
         </Box>
